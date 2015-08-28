30eabb59d5903cf4eb07e128a70e685ce0a4a338
jav219 <jav219@lehigh.edu>
2015-08-28 13:53:16 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+//Jacob Vikse
+//Lab01
+//Hello World Java Program
+
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("#                       #");
+        System.out.println("#   Hello World 2015    #");
+        System.out.println("#                       #");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("test");
+        
+        //test for errors
+    }
+}
