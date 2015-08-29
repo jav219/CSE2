2efd5df8215cd8db65b430c8e19f7a709b26ae95
jav219 <jav219@lehigh.edu>
2015-08-29 20:01:42 +0000

    completed hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//Jacob Vikse
+//August 29, 2015
+//CSE 002 Section: 310
+//hw01
+//Welcome Class Java Program
+
+//class
+public class WelcomeClass{
+    
+    //main method
+    public static void main(String[] args) {
+        
+        //display message with ID signature
+          System.out.println("    -----------          ");
+          System.out.println("    | WELCOME |          ");
+          System.out.println("    -----------          ");
+          System.out.println("  ^  ^  ^  ^  ^  ^       ");
+          System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+          System.out.println("|-J--A--V--2--1--9-|     ");
+          System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+          System.out.println("  v  v  v  v  v  v       ");
+          System.out.println("This July I went scuba diving in the Gulf near");
+          System.out.println("Cancun and cave-diving in the Cenotes");
+       
+    }
+}
